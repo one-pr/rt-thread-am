a3c43b055c0b662cdcfd740198c2bb18b3841271
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-10-22 23:26:39 +0800

    rv: 可用的调试宏定义

--- bsp/abstract-machine/include/extra.h
+++ bsp/abstract-machine/include/extra.h
@@ -1,3 +1,14 @@
 typedef long suseconds_t;
 typedef unsigned long useconds_t;
 #include <sys/types.h>
+
+
+// 调试
+
+#define RT_DEBUG
+
+// #define RT_DEBUG_INIT 1
+// #define RT_DEBUG_THREAD 1
+
+// #define RT_DEBUG_SCHEDULER 1
+// #define RT_DEBUG_TIMER 1
